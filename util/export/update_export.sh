f72688a11c285e703445875420c2350d5340b173
ymaksyuta <ymaksyuta@gmail.com>
2026-08-24 20:17:31 +0300

    Updated repo structure, corrected file modes

---
+++ util/export/update_export.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -r 'export'
+./create_export.sh 'export' '..' '*.git*' 'node_modules' 'export'
---
+++ util/update_manifest.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./create_file_list.sh .. '*.git*' '*node_modules*' > manifest.txt
